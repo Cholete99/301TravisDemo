92639c2ed97edb2c28661fea9f2bb4cf436c3637
Cholete99 <union@ualberta.ca>
2020-10-29 19:28:10 -0600

    added test that fails

--- app/src/test/java/com/example/simpleparadox/listycity/CityListTest.java
+++ app/src/test/java/com/example/simpleparadox/listycity/CityListTest.java
@@ -93,4 +93,10 @@ class CityListTest {
 
         assertEquals(1, cityList.countCities());
     }
+
+    @Test
+    void returnEdmonton() {
+        CityList cityList = mockCityList();
+        assertEquals("Edmonton", cityList.returnEdmonton());
+    }
 }
